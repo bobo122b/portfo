c4d5efb0f0c7cbac9261c5767179e96a472ad620
bobo122b <71796506+bobo122b@users.noreply.github.com>
2020-09-24 00:53:00 +0200

    Add files via upload

---
+++ server.py
@@ -0,0 +1,35 @@
+import csv
+from flask import Flask, render_template, request, redirect
+app = Flask(__name__)
+print(__name__)
+
+@app.route('/')
+def my_home():
+    return render_template('index.html')
+
+
+@app.route('/<string:route>')
+def go(route):
+    return render_template(route)
+
+
+def write_to_csv(data):
+    with open('database.csv', mode='a', newline='') as database2:
+        email = data['email']
+        subject = data['subject']
+        message = data['message']
+        csv_writer = csv.writer(database2, delimiter=',', quotechar='"', quoting = csv.QUOTE_MINIMAL)
+        csv_writer.writerow([email, subject, message])
+
+
+@app.route('/submit_form', methods=['POST', 'GET'])
+def submit_form():
+    if request.method == 'POST':
+        try:
+            data = request.form.to_dict()
+            write_to_csv(data)
+            return redirect('thankyou.html')
+        except:
+            return 'did not save to database'
+    else:
+        return 'something went wrong, try again.'
